1df8e8b8854d4fd0c43e83fceadfb4e2d77be0cf
PG00597789 <PG00597789@INPUHJPC09011.TechMahindra.com>
2019-01-22 16:16:55 +0530

    Removed beans and changed window closing variable.

--- src/com/moon/view/Plan.java
+++ src/com/moon/view/Plan.java
@@ -46,7 +46,7 @@ public class Plan {
 	private void initialize() {
 		frame = new JFrame();
 		frame.setBounds(100, 100, 450, 300);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		
 		JLabel lblNewLabel_2 = new JLabel("Plan Details");
